f1d4a25025d44e1c2a957f9546e6fd56e413b30a
Marcos <marcosnery.comp@gmail.com>
2021-03-22 19:07:21 -0300

    final ncurses adjustments

--- src/menu.c
+++ src/menu.c
@@ -4,53 +4,45 @@
 #define HEIGHT 20
 
 WINDOW *window;
-int ask_reference;
 
 char *options[] = {
 	"Inserir referência manualmente",
 	"Pegar referência pelo potenciometro",
 	"Sair :(",
 };
-
-int startx = 0;
-int starty = 0;
+int ask_reference=0,startx = 0,starty = 0;
 int n_options = sizeof(options) / sizeof(char *);
 
-
-void set_reference() {
-  WINDOW *reference_window;
+void inputReference() {
+  WINDOW *inputWindow;
 
   float reference;
   int max_height, max_width;
   int _startx, _starty;
-
-  init_pair(1, COLOR_GREEN, COLOR_BLACK);
   getmaxyx(stdscr, max_height, max_width);
 
   _starty = (LINES - max_height) / 2;   
   _startx = (COLS - max_width) / 2; 
   refresh();
 
-  reference_window = newwin(HEIGHT, WIDTH, _starty, _startx);
+  inputWindow = newwin(HEIGHT, WIDTH, _starty, _startx);
 
-  wborder(reference_window, ACS_VLINE, ACS_VLINE,ACS_HLINE,ACS_HLINE, ACS_ULCORNER, ACS_URCORNER, ACS_LLCORNER, ACS_LRCORNER);
+  wborder(inputWindow, ACS_VLINE, ACS_VLINE,ACS_HLINE,ACS_HLINE, ACS_ULCORNER, ACS_URCORNER, ACS_LLCORNER, ACS_LRCORNER);
 
-  wattron(reference_window, COLOR_PAIR(1));
-  mvwprintw(reference_window, _starty + 1, _startx + 1, "Digite a temperatura: ");
-  wattroff(reference_window, COLOR_PAIR(1));
+  mvwprintw(inputWindow, _starty + 1, _startx + 1, "Digite a temperatura desejada, usando ponto como separador: ");
 
   echo();
-  wmove(reference_window, _starty + 1, _startx + 13);
-  wscanw(reference_window, "%f", &reference);
+  wmove(inputWindow, _starty + 1, _startx + 13);
+  wscanw(inputWindow, "%f", &reference);
   
   setUserDefined(reference);
 
   ask_reference = 0;
 
-  clear_menu(reference_window);
+  clearWindow(inputWindow);
 }
 
-void print_menu(WINDOW *window, int highlight) {
+void showMenu(WINDOW *window, int highlight) {
 	int x = 2, y = 2, i;
 
 	box(window, 0, 0);
@@ -67,18 +59,15 @@ void print_menu(WINDOW *window, int highlight) {
 	wrefresh(window);
 }
 
-void clear_menu(WINDOW *window_param) {
-	wclear(window_param);
-	wborder(window_param, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');  
-	wrefresh(window_param);
-	delwin(window_param); 
+void clearWindow(WINDOW *window) {
+	wclear(window);
+	wborder(window, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');  
+	wrefresh(window);
+	delwin(window); 
 }
 
 void printToTerminal(TempInfo temp, int controle) {
-  if (ask_reference == 1) {
-    return;
-  }
-
+  if (ask_reference == 1)return;
 
   mvwprintw(window, 6, 1, "\tTEMP INTERNA: %.2f TEMP EXTERNA: %.2f", temp.intTemperature, temp.extTemperature);
   mvwprintw(window, 7, 1, "\tTEMP REFERENCIA: %.2f SINAL DE CONTROLE: %d", temp.refTemperature, controle);
@@ -100,7 +89,6 @@ void printToTerminal(TempInfo temp, int controle) {
 
 void menu() {
 	int highlight = 1;
-	int option = 0;
 	int c;
 
 	initscr();
@@ -115,7 +103,7 @@ void menu() {
 	refresh();
  
 	do {
-		print_menu(window, highlight);
+		showMenu(window, highlight);
 		c = wgetch(window);
 
 		switch(c) {
@@ -132,20 +120,19 @@ void menu() {
 					++highlight;
 				break;
 			case 10:    
-				option = highlight;
-				if (option == 1) {
-					clear_menu(window);
+				if (highlight == 1) {
+					clearWindow(window);
                     ask_reference = 1;
-                     set_reference();
+                    inputReference();
 					window = newwin(HEIGHT, WIDTH, starty, startx);
 					keypad(window, TRUE);
 					refresh();
 				}
-				if (option ==2){
+				else if (highlight ==2){
 					setUserDefined(-1);
 				}
 
-				if (option == 3) {
+				else if (highlight == 3) {
 					quit();
 				}
 
